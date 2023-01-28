2bdddca4791275d82827d31d0006b363c8486afe
Edward Rogachkov <ctz3n4cs@gmail.com>
2023-01-28 13:53:07 +0300

    init commit

---
+++ src/main/java/com/skfl/zuzextesttask/ZuzexTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.skfl.zuzextesttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuzexTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuzexTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skfl/zuzextesttask/ZuzexTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skfl.zuzextesttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuzexTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
